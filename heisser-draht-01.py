f0a84c54867ee2503c64d15ec69b781e72ec1fe5
Olav Schettler <olav@schettler.net>
2017-04-10 22:15:08 +0200

    1. Version: Berührungen

---
+++ heisser-draht-01.py
@@ -0,0 +1,33 @@
+# heisser-draht-01.py
+
+from microbit import *
+
+while True:
+    if button_a.was_pressed():
+
+        display.show(Image(
+            "99909:"
+            "90909:"
+            "90909:"
+            "90999:"
+            "90000"
+        ))
+        sleep(1000)
+
+        beruehrt = 0
+        display.show(str(beruehrt))
+
+        while True:
+            if pin0.is_touched():
+                display.show(Image(
+                    "90009:"
+                    "09090:"
+                    "00900:"
+                    "09090:"
+                    "90009"
+                ))
+                sleep(1000)
+
+                beruehrt += 1
+
+                display.show(str(beruehrt))
