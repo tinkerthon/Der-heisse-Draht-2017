9e68fa532662198bb67133fd37786959ed73f10b
Olav Schettler <olav@schettler.net>
2017-04-22 12:30:53 +0200

    Leichteres Spiel

--- pong_classes.py
+++ pong_classes.py
@@ -34,11 +34,11 @@ class Ball:
     '''
     
     trails = [
-        [2, 2, 1, 1, 0],
+        [2, 2, 1, 0, 0],
         [2, 2, 1, 1, 1],
         [2, 2, 2, 2, 2],
         [2, 2, 3, 3, 3],
-        [2, 2, 3, 3, 4]
+        [2, 2, 3, 4, 4]
     ]
     
     def run(self):
@@ -72,7 +72,7 @@ class Game:
         self.ball.run()
         self.bat.run()
         print("Round:", self.round, "Ball:", self.ball.y, "Bat:", self.bat.y)
-        sleep(300)
+        sleep(400)
 
     
     def run(self):
